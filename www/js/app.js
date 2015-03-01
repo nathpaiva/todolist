2a6616eb5bbce893f5c84f64683dec5132df3635
Nath Paiva <nathaliafpaiva@gmail.com>
2015-03-01 18:56:30 -0300

    adjustment for check and uncheck tasks done

--- www/js/app.js
+++ www/js/app.js
@@ -9,44 +9,43 @@
 var taskList = new Array();
 
 $(document).ready(function(){
-	var $newTaskInput = $("#newTaskInput");
-	var $taskList = $("#taskList");
-	var taskThouchStart;
-	var taskThouchEnd;
-	var taskThouchStartX;
-	var taskThouchEndX;
+// localStorage.clear();
+var $newTaskInput = $("#newTaskInput");
+var $taskList = $("#taskList");
+var taskThouchStart;
+var taskThouchEnd;
+var taskThouchStartX;
+var taskThouchEndX;
 
-	if( window.localStorage ) {
-		taskList = JSON.parse(window.localStorage.getItem("taskList"));
-	}
+if( window.localStorage ) {
+	taskList = JSON.parse(window.localStorage.getItem("taskList"));
+}
 
-	if( null !== taskList ){
-		for (var i = 0; i < taskList.length; i++) {
-			taskList[i];
-			var newTask = "<li data-key='"+ taskList[i].key +"'>" + taskList[i].task + "</li>";
-			$taskList.append(newTask);
-		};
-	} else{
-		taskList = new Array();
-	}
+if( null !== taskList ){
+	for (var i = 0; i < taskList.length; i++) {
+		var newTask = "<li data-key='"+ taskList[i].key +"' class='"+ taskList[i].done +"'>" + taskList[i].task + "</li>";
+		$taskList.append(newTask);
+	};
+} else{
+	taskList = new Array();
+}
 
 	// action buttom
 	$("#addNewTask").on("click", function(){
 		var key = Date.now();
 
 		if($newTaskInput.val() != ""){
-			var newTask = "<li data-key='"+ key +"'>" + $newTaskInput.val() + "</li>";
+			var newTask = "<li data-key='"+ key +"' done='false'>" + $newTaskInput.val() + "</li>";
 			$taskList.append(newTask);
 
 			taskList.push({
 				key:key,
 				task: $newTaskInput.val(),
-				done: false
+				done: "none"
 			});
 			if(window.localStorage){
 				window.localStorage.setItem("taskList" , JSON.stringify(taskList));
 			}
-			console.log("aqui");
 			$newTaskInput.val("");
 		}
 	});
@@ -62,17 +61,38 @@ $(document).ready(function(){
 		var $this = $(this);
 		var end = document.elementFromPoint(e.originalEvent.touches[0].pageX, e.originalEvent.touches[0].pageY);
 		$end = $(end);
-
 		taskThouchEnd = $end.attr("data-key");
 		taskThouchEndX =  e.originalEvent.touches[0].pageX;
 
 		if( taskThouchStart === taskThouchEnd ){
 			if( taskThouchStartX < taskThouchEndX ){
 				if($this.hasClass("done")){
-					$this.removeClass("done");
+					$this.removeClass("done").addClass("none");
 				} else{
-					$this.addClass("done");
+					$this.addClass("done").removeClass("none");
 				}
+
+				upDateList = jQuery.map( taskList, function( val, i ) {
+					var doneTaskt;
+					if(val.key == taskThouchEnd){
+						if($this.attr("class") == "done"){
+							doneTaskt = "done";
+						} else{
+							doneTaskt = "none";
+						}
+					} else{
+						doneTaskt = val.done;
+					}
+					return valor = {
+						key: val.key,
+						task: val.task,
+						done: doneTaskt
+					};
+				});
+				if(window.localStorage){
+					window.localStorage.setItem("taskList" , JSON.stringify(upDateList));
+				}
+
 			} else{
 				var confirmation = confirm("Do you want to remove this task?");
 
