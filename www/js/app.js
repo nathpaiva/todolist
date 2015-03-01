a21f9e1ab56d80913f8cb2413a96873fc12ce4b7
Nath Paiva <nathaliafpaiva@gmail.com>
2015-03-01 15:40:06 -0300

    saving the list in localstorage

--- www/js/app.js
+++ www/js/app.js
@@ -6,6 +6,7 @@
 // 		var n = d.getTime();
 // 	}
 // };
+var taskList = new Array();
 
 $(document).ready(function(){
 	var $newTaskInput = $("#newTaskInput");
@@ -15,13 +16,37 @@ $(document).ready(function(){
 	var taskThouchStartX;
 	var taskThouchEndX;
 
+	if( window.localStorage ) {
+		taskList = JSON.parse(window.localStorage.getItem("taskList"));
+	}
+
+	if( null !== taskList ){
+		for (var i = 0; i < taskList.length; i++) {
+			taskList[i];
+			var newTask = "<li data-key='"+ taskList[i].key +"'>" + taskList[i].task + "</li>";
+			$taskList.append(newTask);
+		};
+	} else{
+		taskList = new Array();
+	}
+
 	// action buttom
 	$("#addNewTask").on("click", function(){
 		var key = Date.now();
 
 		if($newTaskInput.val() != ""){
-			var newTask = "<li data-key='"+ key +"'>" + $newTaskInput.val() + "</li>"
+			var newTask = "<li data-key='"+ key +"'>" + $newTaskInput.val() + "</li>";
 			$taskList.append(newTask);
+
+			taskList.push({
+				key:key,
+				task: $newTaskInput.val(),
+				done: false
+			});
+			if(window.localStorage){
+				window.localStorage.setItem("taskList" , JSON.stringify(taskList));
+			}
+			console.log("aqui");
 			$newTaskInput.val("");
 		}
 	});
@@ -53,6 +78,15 @@ $(document).ready(function(){
 
 				if( confirmation == true ){
 					$end.remove();
+
+					taskList = $.grep(taskList, function(e){
+						return e.key != taskThouchEnd;
+					});
+
+					if(window.localStorage){
+						window.localStorage.setItem("taskList" , JSON.stringfy(taskList));
+					}
+
 					return true;
 				}else{
 					return false;
